3718ed1d7fe0aa58f3b1ee8ecbb29c587337f2df
Ivan <efir84@gmail.com>
2020-10-01 16:01:30 +0300

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let money = +prompt('Ваш бюджет на месяц?', '');
 let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-let mandatoryExpenses = +prompt('Введите обязательную статью расходов в этом месяце', '');
-let cost = +prompt('Во сколько обойдется', '');
+// let mandatoryExpenses = prompt('Введите обязательную статью расходов в этом месяце', '');
+// let cost = +prompt('Во сколько обойдется', '');
 
 let appData = {
     budget: money,
@@ -16,6 +16,16 @@ let appData = {
     income: [],
     savings: false
 };
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+console.log(appData.expenses);
 
 let budgetDay = money / 30;
 
